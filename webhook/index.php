f9e640805aad23a34656d2f948030d38ebaa4201
Inggo Espinosa <inggo.espinosa@gmail.com>
2017-08-01 14:12:12 +0000

    Test webhook

---
+++ webhook/index.php
@@ -0,0 +1,10 @@
+<?php
+
+use \unreal4u\TelegramAPI\Telegram\Types\Update;
+
+$updateData = json_decode(file_get_contents('php://input'), true);
+
+$update = new Update($updateData);
+
+// Check update for command
+file_put_contents('test.in', $update);
