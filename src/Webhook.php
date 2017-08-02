42f093bf3a1f61f8fab03164f2b0f5d4bd9a7931
Inggo Espinosa <inggo.espinosa@gmail.com>
2017-08-02 11:55:12 +0000

    Fixes

--- src/Webhook.php
+++ src/Webhook.php
@@ -13,9 +13,6 @@ class Webhook
     {
         $request = json_decode(file_get_contents('php://input'), true);
         $this->update = new Update($request);
-
-        file_put_contents('.lastupdate', $this->update->update_id);
-
         $this->command = new Command($this->update);
     }
 }
