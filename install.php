a4ba6229b200c8d9168c477d9a716b2b33a77de9
Inggo E <inggo.espinosa@gmail.com>
2019-03-03 11:16:08 +0800

    Update renew script

--- install.php
+++ install.php
@@ -6,7 +6,7 @@ use \unreal4u\TelegramAPI\TgLog;
 use \unreal4u\TelegramAPI\Telegram\Methods\SetWebhook;
 
 $setWebhook = new SetWebhook();
-$setWebhook->url = 'https://inggo.xyz/bndo-bot/' . BOT_TOKEN . '/hook.php';
+$setWebhook->url = SITE_ROOT . BOT_TOKEN . '/hook.php';
 
 $tgLog = new TgLog(BOT_TOKEN);
 $tgLog->performApiRequest($setWebhook);
